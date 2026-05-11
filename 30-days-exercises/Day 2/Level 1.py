600312cc12b80e85b4cd35078057ec27080b30d9
Feintwell <167493918+Feintwell@users.noreply.github.com>
2026-05-11 14:07:07 +1200

    Add files via upload

---
+++ 30-days-exercises/Day 2/Level 1.py
@@ -0,0 +1,15 @@
+# Day 2: 30 Days of python programming
+
+firstname = "Scone"
+lastname = "Pastry"
+fullname = "Scone Pastry"
+country = "Kingdom"
+city = "Palace"
+age = 2
+year = 2026
+is_married = False
+is_true = "orange"
+is_light_on = True
+nails, fangs, fur = "sharp", "white", "soft"
+
+print(firstname, lastname, fullname, country, city, age, year, is_married, is_true, is_light_on, nails, fur, fangs)
