e28ce99442bca27fdad0492765ce3d34625420d5
Feintwell <167493918+Feintwell@users.noreply.github.com>
2026-05-28 14:59:38 +1200

    Add files via upload

---
+++ 30-days-exercises/Day 4/test.py
@@ -0,0 +1,18 @@
+# 8 + 6 = 14
+# 8 - 6 = 2
+# 8 * 6 = 48
+# 8 / 6 = 1.33
+# 8 % 6 = 2
+# 8 // 6 = 1
+# 8 ** 6 = 262144
+
+q = 8
+w = 6
+
+print("{} + {} = {}".format(q, w, q + w))
+print("{} - {} = {}".format(q, w, q - w))
+print("{} * {} = {}".format(q, w, q * w))
+print("{} / {} = {:.2f}".format(q, w, q / w))
+print("{} % {} = {}".format(q, w, q % w))
+print("{} // {} = {}".format(q, w, q // w))
+print("{} ** {} = {}".format(q, w, q ** w))
