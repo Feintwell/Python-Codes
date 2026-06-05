c02c6862b5e4364bc95f4f369681f0526ad8ae80
Feintwell <167493918+Feintwell@users.noreply.github.com>
2026-06-06 09:43:58 +1200

    Add files via upload

---
+++ NumberGuessingGame.py
@@ -0,0 +1,17 @@
+import random
+
+mine = random.randint(1, 10)
+
+guess = int(input("Guess a number between 1 to 10 "))
+
+if guess == mine:
+    print("You have it right \n You win \n Congratulations!")
+    print("The number was: ", mine)
+elif guess < 5:
+    print("Close, dear, but it is smaller than your number")
+    print("The number was: ", mine)
+elif guess > 5:
+    print("Close, dear, but it is bigger than your number")
+    print("The number was: ", mine)
+else:
+    print("Sorry dear")
