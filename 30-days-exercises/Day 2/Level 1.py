bb08cf1f76d34bc2d36f34fe8f027a821801424a
Feintwell <167493918+Feintwell@users.noreply.github.com>
2026-07-18 09:14:55 +1200

    Update Level 1.py

--- 30-days-exercises/Day 2/Level 1.py
+++ 30-days-exercises/Day 2/Level 1.py
@@ -1,15 +1,37 @@
+#Write a python comment saying 'Day 2: 30 Days of python programming'
 # Day 2: 30 Days of python programming
 
+#Declare a first name variable and assign a value to it
 firstname = "Scone"
+
+#Declare a last name variable and assign a value to it
 lastname = "Pastry"
-fullname = "Scone Pastry"
-country = "Kingdom"
-city = "Palace"
-age = 2
+
+#Declare a full name variable and assign a value to it
+fullname = "Taylor Swift"
+
+#Declare a country variable and assign a value to it
+country = "Greece"
+
+#Declare a city variable and assign a value to it
+city = "New Orleans"
+
+#Declare an age variable and assign a value to it
+age = 479
+
+#Declare a year variable and assign a value to it
 year = 2026
-is_married = False
+
+#Declare a variable is_married and assign a value to it
+is_married = True
+
+#Declare a variable is_true and assign a value to it
 is_true = "orange"
-is_light_on = True
+
+#Declare a variable is_light_on and assign a value to it
+is_light_on = False
+
+#Declare multiple variable on one line
 nails, fangs, fur = "sharp", "white", "soft"
 
 print(firstname, lastname, fullname, country, city, age, year, is_married, is_true, is_light_on, nails, fur, fangs)
